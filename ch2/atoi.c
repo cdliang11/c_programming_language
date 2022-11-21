523bfc67858e1ec3d2b3ffc1e6931437061d68ca
cdliang11 <1404056823@qq.com>
2022-11-21 18:44:38 +0800

    google c style

--- ch2/atoi.c
+++ ch2/atoi.c
@@ -3,11 +3,11 @@
 
 
 //int atoi(char str[]) {
-//	int i, n = 0;
-//	for (i = 0; str[i] >= '0' && str[i] <= '9'; i++) {
-//		n = 10 * n + (str[i] - '0');
-//	}
-//	return n;
+//  int i, n = 0;
+//  for (i = 0; str[i] >= '0' && str[i] <= '9'; i++) {
+//    n = 10 * n + (str[i] - '0');
+//  }
+//  return n;
 //}
 
 
@@ -15,27 +15,27 @@
 #define INT_MIN (-INT_MAX - 1)
 
 int atoi(char *str) {
-	assert(str); // 空指针 
-	if (*str == '\0') return 0;
-	while (*str == ' ') str++;
-	int flag = 1; // 正负标志
-	if (*str == '+') str++;
-	else if (*str == '-') {
-		flag = -1;
-		str++;
-	} 
-	long long n = 0;
-	while (*str >= '0' && *str <= '9') {
-		n = n * 10 + flag * (*str - '0');
-		if (n > INT_MAX || n < INT_MIN) return  0;
-		str++;
-	}
-	if (*str == '\0') return (int)n;
-	return (int)n;
+  assert(str);
+  if (*str == '\0') return 0;
+  while (*str == ' ') str++;
+  int flag = 1;
+  if (*str == '+') str++;
+  else if (*str == '-') {
+    flag = -1;
+    str++;
+  } 
+  long long n = 0;
+  while (*str >= '0' && *str <= '9') {
+    n = n * 10 + flag * (*str - '0');
+    if (n > INT_MAX || n < INT_MIN) return  0;
+    str++;
+  }
+  if (*str == '\0') return (int)n;
+  return (int)n;
 }
 
 int main(){
-	char str[3] = {'-', '1', '2'};
-	printf("%d", atoi(str));
-	return 0;
+  char str[3] = {'-', '1', '2'};
+  printf("%d", atoi(str));
+  return 0;
 }
--- ch3/binsearch.c
+++ ch3/binsearch.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 
 int binsearch(int x, int v[], int n) {
-	int low = 0, high = n - 1, mid; 
-	
-	while (low < high) {
-		mid = (low + high) / 2;
-		// 相比于原始减少了一次判断 
-		printf("%d,%d,%d\n", v[low], v[high], v[mid]); 
-		if (x <= v[mid]) {
-			high = mid;
-		}
-		else {
-			low = mid+1;
-		}
-	} 
-	return v[low] == x ? low : -1;
+  int low = 0, high = n - 1, mid; 
+  
+  while (low < high) {
+    mid = (low + high) / 2;
+    printf("%d,%d,%d\n", v[low], v[high], v[mid]); 
+    if (x <= v[mid]) {
+      high = mid;
+    }
+    else {
+      low = mid+1;
+    }
+  }
+  return v[low] == x ? low : -1;
 }
 
 int main() {
-	int x = -1;
-	int v[8] = {0,1,2,3,4,5,6,7};
-	int n = 8;
-	printf("%d", binsearch(x, v, n));
-	return 0;
+  int x = -1;
+  int v[8] = {0,1,2,3,4,5,6,7};
+  int n = 8;
+  printf("%d", binsearch(x, v, n));
+  return 0;
 }
