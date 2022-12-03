ab3c8e578af084c546b807be1cfcf314519521b3
cdliang11 <1404056823@qq.com>
2022-12-03 17:18:25 +0800

    qsort

---
+++ ch5/sort.c
@@ -0,0 +1,105 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+
+char *lineptr[MAXLINES];  // 只想文本行的指针数组
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort(char *lineptr[], int left, int right);
+
+main() {
+  int nlines;
+  if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+    qsort(lineptr, 0, nlines - 1);
+    writelines(lineptr, nlines);
+    return 0;
+  } else {
+    printf("error: input too big to sort\n");
+    return 1;
+  }
+}
+
+#define MAXLEN 1000    // 每行的最大长度
+
+int getline(char s[], int lim);
+
+
+int getline(char s[], int lim) {
+  int c, i;
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    s[i] = c;
+  }
+  if (c == '\n') {
+    s[i] = c;
+    i++;
+  }
+  s[i] = '\0';
+  return i;
+}
+
+#define ALLOCSIZE 10000
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+
+char *alloc(int n) {
+  if (allocbuf + ALLOCSIZE - allocp >= n) {
+    allocp += n;
+    return allocp - n;
+  } else {
+    return 0;
+  }
+}
+
+void afree(char *p) {
+  if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+    allocp = p;
+}
+
+int readlines(char *lineptr[], int maxlines) {
+  int len, nlines;
+  char *p, line[MAXLEN];
+  nlines = 0;
+  while((len = getline(line, MAXLEN)) > 0) {
+    if (nlines >= MAXLINES || (p = alloc(len)) == NULL) {
+      return -1;
+    } else {
+      line[len-1] = '\0';
+      strcpy(p, line);
+      lineptr[nlines++] = p;
+    }
+  }
+  return nlines;
+}
+
+void writelines(char *lineptr[], int nlines) {
+  int i;
+  for (i = 0; i < nlines; i++) {
+    printf("%s\n", lineptr[i]);
+  }
+}
+
+void qsort(char *v[], int left, int right) {
+  int i, last;
+  void swap(char *v[], int i, int j);
+  if (left >= right) return;
+  swap(v, left, (left + right) / 2);
+  last = right;
+  for (i = left + 1; i <= right; i++) {
+    if (strcmp(v[i], v[left]) < 0)
+      swap(v, ++last, i);
+  }
+  swap(v, left, right);
+  qsort(v, left, last - 1);
+  qsort(v, last + 1, right);
+}
+
+void swap(char *v[], int i, int j) {
+  char *tmp;
+  tmp = v[i];
+  v[i] = v[j];
+  v[j] = tmp;
+}
