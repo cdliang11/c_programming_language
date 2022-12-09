09bc262184e0be65628cb7170704cd636abf60ae
cdliang11 <1404056823@qq.com>
2022-12-09 18:10:18 +0800

    fix sort

--- ch5/sort.c
+++ ch5/sort.c
@@ -14,7 +14,6 @@ void qsort(char *lineptr[], int left, int right);
 int main() {
   int nlines;
   if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-    writelines(lineptr, nlines);
     qsort(lineptr, 0, nlines - 1);
     writelines(lineptr, nlines);
     return 0;
@@ -86,12 +85,12 @@ void qsort(char *v[], int left, int right) {
   void swap(char *v[], int i, int j);
   if (left >= right) return;
   swap(v, left, (left + right) / 2);
-  last = right;
+  last = left;
   for (i = left + 1; i <= right; i++) {
     if (strcmp(v[i], v[left]) < 0)
       swap(v, ++last, i);
   }
-  swap(v, left, right);
+  swap(v, left, last);
   qsort(v, left, last - 1);
   qsort(v, last + 1, right);
 }
